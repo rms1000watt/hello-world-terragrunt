f15c033d6435003a3412579cbf764da000a11ab0
Ryan Smith <rms1000watt@gmail.com>
2018-04-19 08:41:32 -0700

    updated create-app with symlink

--- orgs/create-app.sh
+++ orgs/create-app.sh
@@ -1,32 +1,22 @@
 #!/usr/bin/env bash
 
 create-app() {
-  path=$1
+  org_name=$1
+  environment=$2
+  app_name=$3
+  path="$1/$2/$3"
 
   echo "App location: $path"
 
   if [ ! -d "$path" ]; then
     echo "Making directories: $path"
-    mkdir -p "$path"
+    mkdir -p "$path" || exit 1
   fi
 
-  file="$path/main.tf"
-  if [ ! -f "$file" ]; then
-    echo "Creating file: $file"
-    cat << EOF > "$file"
-variable "org_name" {}
-variable "aws_region" {}
-variable "environment" {}
-
-provider "aws" {
-  region = "\${var.aws_region}"
-}
-
-terraform {
-  backend "s3" {}
-}
-EOF
-  fi
+  wd=$(pwd)
+  cd "$path" || exit 1
+  ln -s ../../main.tf
+  cd "$wd" || exit 1
 
   file="$path/terraform.tfvars"
   if [ ! -f "$file" ]; then
@@ -39,14 +29,10 @@ terragrunt {
 }
 EOF
   fi
-
-  # file="$path/$(basename "$path").tf"
-  # if [ ! -f "$file" ]; then
-  #   echo "Creating file: $file"
-  #   echo "// TODO: Put app definition here" > "$file"
-  # fi
 }
 
 
-if [ -z "$1" ]; then echo "ERROR: App path not specified. Exiting.." && exit 1; fi
-create-app "$1"
+if [ -z "$1" ]; then echo "ERROR: \$1 (org_name) not specified. Exiting.."    && exit 1; fi
+if [ -z "$2" ]; then echo "ERROR: \$2 (environment) not specified. Exiting.." && exit 1; fi
+if [ -z "$3" ]; then echo "ERROR: \$3 (app_name) not specified. Exiting.."    && exit 1; fi
+create-app "$1" "$2" "$3"
