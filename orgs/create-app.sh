c513a001490801edcd6dc449e7cc7db20a3fc9b8
Ryan M Smith <rms1000watt@users.noreply.github.com>
2018-08-31 09:04:43 -0700

    Update create-app.sh

--- orgs/create-app.sh
+++ orgs/create-app.sh
@@ -9,20 +9,25 @@ create-app() {
   app_name=$4
 
   path="$org_name/$region/$environment/$app_name"
-  if [ "$region" = "$region_global" ]; then
+  envPath="$org_name/$region/$environment"
+  regionPath="$org_name/$region"
+
+  if [[ $region = $region_global ]]; then
     path="$org_name/$region/$app_name"
+    envPath="$org_name/$region"
+    regionPath="$org_name/$region"
   fi
 
   echo "App location: $path"
 
-  if [ ! -d "$path" ]; then
+  if [[ ! -d $path ]]; then
     echo "Making directories: $path"
     mkdir -p "$path" || exit 1
   fi
 
   wd=$(pwd)
   cd "$path" || exit 1
-  if [ "$region" = "$region_global" ]; then
+  if [[ $region = $region_global ]]; then
     ln -s ../../../main.tf
   else
     ln -s ../../../../main.tf
@@ -30,7 +35,7 @@ create-app() {
   cd "$wd" || exit 1
 
   file="$path/terraform.tfvars"
-  if [ ! -f "$file" ]; then
+  if [[ ! -f $file ]]; then
     echo "Creating file: $file"
     cat << EOF > "$file"
 terragrunt {
@@ -40,11 +45,32 @@ terragrunt {
 }
 EOF
   fi
+
+  if [[ $region = $region_global ]]; then
+    region="us-west-2"
+  fi
+
+  file="$regionPath/region.tfvars"
+  if [[ ! -f $file ]]; then
+    echo "Creating file: $file"
+    cat << EOF > "$file"
+aws_region = "${region}"
+EOF
+  fi
+
+  file="$envPath/env.tfvars"
+  if [[ ! -f $file ]]; then
+    echo "Creating file: $file"
+    cat << EOF > "$file"
+environment = "${environment}"
+EOF
+  fi
+
 }
 
+if [[ -z $1 ]]; then echo "ERROR: \$1 (org_name) not specified. Exiting.."    && exit 1; fi
+if [[ -z $2 ]]; then echo "ERROR: \$2 (region) not specified. Exiting.."      && exit 1; fi
+if [[ -z $3 ]]; then echo "ERROR: \$3 (environment) not specified. Exiting.." && exit 1; fi
+if [[ -z $4 ]]; then echo "ERROR: \$4 (app_name) not specified. Exiting.."    && exit 1; fi
 
-if [ -z "$1" ]; then echo "ERROR: \$1 (org_name) not specified. Exiting.."    && exit 1; fi
-if [ -z "$2" ]; then echo "ERROR: \$2 (region) not specified. Exiting.."      && exit 1; fi
-if [ -z "$3" ]; then echo "ERROR: \$3 (environment) not specified. Exiting.." && exit 1; fi
-if [ -z "$4" ]; then echo "ERROR: \$4 (app_name) not specified. Exiting.."    && exit 1; fi
 create-app "$1" "$2" "$3" "$4"
